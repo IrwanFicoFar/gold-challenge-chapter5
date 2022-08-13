9b17c60d6425e425343111a30c75df0b7c10c857
IrwanFicoFar <irwanhadi9@gmail.com>
2022-08-13 15:19:32 +0700

    layouting login

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const fs = require('fs')
 
 app.use('/chapter3', express.static(__dirname + '/chapter3'))
 app.use('/chapter4', express.static(__dirname + '/chapter4'))
+app.use('/css', express.static(__dirname + '/css'))
 
 app.get('/', (req, res) => {
     res.send("hey test awal")
@@ -15,6 +17,15 @@ app.get('/chapter3', (req,res) => {
 
 app.get('/chapter4', (req,res) => {
     res.sendFile(path.join(__dirname + '/chapter4/index.html'))
+})
+
+app.get('/user', (req,res) => {
+    let myUser = JSON.parse(fs.readFileSync('user.json', 'utf-8'))
+    res.send(myUser)
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html'))
 })
 
 app.listen(5000)
